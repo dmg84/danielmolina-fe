8cf1c73eb7d533be811cd3a1fb91b1695a185e83
Daniel Molina <daniel.7c0@gmail.com>
2020-04-12 17:07:42 +0200

    create react app

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 'use strict';
 
-var fs = require('fs');
+/*var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'content-type': 'text/html'}
+  };
+
+  callback(null, result);
+};*/
+
+
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = function (event, context, callback) {
+  const contents = fs.readFileSync(`build${path.sep}index.html`);
+  const result = {
     statusCode: 200,
     body: contents.toString(),
     headers: {'content-type': 'text/html'}
